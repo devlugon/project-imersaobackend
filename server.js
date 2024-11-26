c9f4cb8306fe86a9c422be3b8df95748683122ff
Lucas Gonçalves <lucasgnclvs1@gmail.com>
2024-11-26 04:22:33 -0300

    feat: create routes and database connection with mongodb
    The code has been modified to manage new routes and make connection with MongoDB which is a NoSQL option

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
 import express from "express";
+import connectDatabase from "./src/config/DbConfig.js";
+
+await connectDatabase(process.env.STRING_CONNECTION)
 
 const app = express();
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log("server listening...");
 });
 
+app.get("/posts", (req, res) => {
+    res.status(200).send(posts)
+});
+
 app.get("/api", (req, res) => {
     res.status(200).send("Boas vindas à imersão!");
+});
+
+function findPostById(id){
+    return posts.findIndex((post) =>{
+        return post.id === Number(id)
+    })
+}
+
+app.get("/posts/:id", (req, res) => {
+    const index = findPostById(req.params.id)
+    res.status(200).send(posts[index])
 });
