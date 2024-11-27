6d770b459aeec7a9eb4aa797472114c005556292
Lucas Gonçalves <lucasgnclvs1@gmail.com>
2024-11-27 03:02:31 -0300

    feat: Create method for upload images
    Creating method for make upload images and association with post id on database

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,4 +1,5 @@
 import { getAllPosts, createPost } from "../models/postsModel.js";
+import fs from "fs";
 
 export async function listPosts(req, res) {
     const posts = await getAllPosts();
@@ -10,6 +11,23 @@ export async function createNewPost(req, res) {
     try {
         const createdPost = await createPost(newPost);
         res.status(200).json(createdPost);  
+    } catch(erro) {
+        console.error(erro.message);
+        res.status(500).json({"Erro":"Falha na requisição"})
+    }
+}
+
+export async function imageUpload(req, res) {
+    const newPost = {
+        descricao: "",
+        imgUrl: req.file.originalname,
+        alt: ""
+    };
+    try {
+        const createdPost = await createPost(newPost);
+        const updatedImage = `uploads/${createdPost.insertedId}.png`
+        fs.renameSync(req.file.path, updatedImage)
+        res.status(200).json(createdPost);
     } catch(erro) {
         console.error(erro.message);
         res.status(500).json({"Erro":"Falha na requisição"})
--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,10 +1,23 @@
 import express from "express";
-import { listPosts, createNewPost } from "../controllers/postsController.js"
+import multer from "multer";
+import { listPosts, createNewPost, imageUpload } from "../controllers/postsController.js"
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+})
+
+const upload = multer({ dest: "./uploads" , storage})
 
 const routes = (app) => {
     app.use(express.json());
     app.get("/posts", listPosts);
     app.post("/posts", createNewPost)
+    app.post("/upload", upload.single("image"), imageUpload)
 }
 
 export default routes;
